f370813186eef17f94df92175d1650e5b959d9d7
Celine <1155176381@link.cuhk.edu.hk>
2023-12-01 02:10:12 +0800

    draft on LoadData

--- src/db/Admin.java
+++ src/db/Admin.java
@@ -108,7 +108,76 @@ public class Admin {
     }
     
     void LoadData() {
-        
+        try{
+            String thisLine;
+            Scanner input = new Scanner(System.in);
+            System.out.println("Type in the Source Data Folder Path: ");
+            String folderName = input.nextLine();
+
+            BufferedReader br = null;
+            br = new BufferedReader(new FileReader("/"+folderName+"category.txt"));
+            while((thisLine = br.readLine()) != null){
+                String[] splited = thisLine.split("\\ ",3);
+                String a = splited[0];
+                String b = splited[1];
+                Statement stmt = conn.createStatement();
+                stmt.executeQuery("INSERT INTO Category VALUES(" + a + "," + b + ")");
+
+            }
+            BufferedReader br1 = null;
+            br1 = new BufferedReader(new FileReader("/"+folderName+"manufacturer.txt"));
+            while((thisLine = br1.readLine()) != null){
+                String[] splited = thisLine.split("\\ ",10);
+                String a = splited[0];
+                String b = splited[1];
+                String c = splited[2];
+                String d = splited[3];
+                Statement stmt = conn.createStatement();
+                stmt.executeQuery("INSERT INTO Manufacturer VALUES(" + a + "," + b + "," + c + "," + d + ")");
+            }
+            BufferedReader br2 = null;
+            br2 = new BufferedReader(new FileReader("/"+folderName+"part.txt"));
+            while((thisLine = br2.readLine()) != null){
+                String[] splited = thisLine.split("\\ ",10);
+                String a = splited[0];
+                String b = splited[1];
+                String c = splited[2];
+                String d = splited[3];
+                String e = splited[4];
+                String f = splited[5];
+                String g = splited[6];
+                Statement stmt = conn.createStatement();
+                stmt.executeQuery("INSERT INTO Part VALUES(" + a + "," + b + "," + c + "," + d + "," + e + "," + f + "," + g + ")");
+            }
+            BufferedReader br3 = null;
+            br3 = new BufferedReader(new FileReader("/"+folderName+"salesperson.txt"));
+            while((thisLine = br3.readLine()) != null){
+                String[] splited = thisLine.split("\\ ",10);
+                String a = splited[0];
+                String b = splited[1];
+                String c = splited[2];
+                String d = splited[3];
+                Statement stmt = conn.createStatement();
+                stmt.executeQuery("INSERT INTO Salesperson VALUES(" + a + "," + b + "," + c + "," + d + ")");
+            }
+            BufferedReader br4 = null;
+            br4 = new BufferedReader(new FileReader("/"+folderName+"manufacturer.txt"));
+            while((thisLine = br4.readLine()) != null){
+                String[] splited = thisLine.split("\\ ",10);
+                String a = splited[0];
+                String b = splited[1];
+                String c = splited[2];
+                String d = splited[3];
+                Statement stmt = conn.createStatement();
+                stmt.executeQuery("INSERT INTO Transaction VALUES(" + a + "," + b + "," + c + "," + d + ")");
+            }
+        } 
+        catch(Exception e) {
+            System.err.println(e);
+            System.err.println("Load data Failed");
+            System.exit(1);
+        }        
+
     }
     
     void ShowTable(String tableName) {
